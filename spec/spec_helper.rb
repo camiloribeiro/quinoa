a402ff968d1f0bcb3d2aa6052c340fb126e7601f
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-09-03 22:42:13 +0200

    Adding codeclimate to webmock white list

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,5 @@ CodeClimate::TestReporter.start
 RSpec.configure do |config|
   config.color = true
   config.formatter     = 'documentation'
+  WebMock.disable_net_connect!(allow: %w{codeclimate.com})
 end
